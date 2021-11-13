41ea89000d02a7db3783398531b13b4cb3fc6bfe
Zach Liibbe <zliibbe@gmail.com>
2021-11-12 20:25:52 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var animal = 'Panda';
+var isCuddly = true;
+animal = 'Dog';
+num8 = 3
+num3 = 24
+var quotient = num3/num8;
+
+favoriteFilms = ["War Horse", "Toy Story", "Juno"];
+
+//remove first element from favoriteFilms array
+favoriteFilms.shift();
+
+//line of code that prints how many characters in animal string
+console.log(animal.length);
