a1f1ad0da3fa13ce2969ad2fc72c68d48525e43f
Zach Liibbe <zliibbe@gmail.com>
2021-11-12 20:45:00 -0700

    Initial commit

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var myHouse = {
+  address = "614 Glen Eyrie Circle",
+  city = "Colorado Springs",
+  houseNumber = 614,
+  numberOfBedrooms = 3,
+  oneLevel = false,
+  leavesRaked = true,
+  peopleInsideOverTime = {3, 2, 3, 2, 1, 5},
+  ownerNames = {"Laura", "Zach"},
+}
