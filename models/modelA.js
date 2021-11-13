ad2beb66bce5dcd412f4c6d5294fe845b7787939
Zach Liibbe <zliibbe@gmail.com>
2021-11-12 20:39:18 -0700

    Initial commit

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var myLaptop = {
+  model: 'MacBook',
+  color: 'silver',
+  year: 2020,
+  useHours: 65,
+  clean: true,
+  hotTemp: false,
+  daysUsed: {'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+  'Friday', 'Saturday', 'Sunday'},
+  monthsUsed: {'October', 'November'},
+}
