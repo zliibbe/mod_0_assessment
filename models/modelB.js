c653ddc0dc6b69350b49affc51cc36ad8ea593d9
Zach Liibbe <zliibbe@gmail.com>
2021-11-12 20:52:58 -0700

    Added time explanation

--- models/modelB.js
+++ models/modelB.js
@@ -8,3 +8,13 @@ var myHouse = {
   peopleInsideOverTime = {3, 2, 3, 2, 1, 5},
   ownerNames = {"Laura", "Zach"},
 }
+
+/*I took a break to be with my daughter and put her to bed (as my spouse
+left town today to care for my brother and sister-in-law) inbetween
+Part 1: Quiz and Part 2: work. I finished the quiz in less
+than 10 minutes and worked on part 2 for about 45 mins. Just want to
+be clear, that although my time was divided I adhered to the time
+requirements. I did not realize these should ideally be done together
+until after I started the quiz. My apologies if I misunderstood the
+timing. However, I was faithful to doing the work within the required
+time, although the time block was divided. Thanks, Zach*/
